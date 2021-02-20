ac936b7923096ee1afd87cf19b70f3bdbdb8138a
KLDudzinsky <kldudzinsky@gmail.com>
2021-02-20 13:46:45 -0800

    added kat map html

--- static/js/logic.js
+++ static/js/logic.js
@@ -13,6 +13,9 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: API_KEY
 }).addTo(myMap);
 
+
+var object = [];
+
 //all feature function 
 function all_features(jsonData) {
     geojson = L.choropleth(jsonData, {
@@ -30,10 +33,11 @@ function all_features(jsonData) {
             layer.on({
                 mouseover: function (event) {
                     layer = event.target;
+                    layer.bindTooltip("<h1>" + feature.properties.NAME + "</h1>" + "<hr></hr>" + "<h2>" + feature.properties.ACRES + "</h2").openTooltip();
                     layer.setStyle({
                         fillOpacity: 0.3
                     });
-                    
+
                 },
                 mouseout: function (event) {
                     layer = event.target;
@@ -48,43 +52,51 @@ function all_features(jsonData) {
             });
         }
 
-    }).addTo(myMap);
+    });
+    return geoJson
 }
-
 
 //state acre info
 d3.json("raw_data/acres_stateLevelGeo.json", function (data) {
-    all_features(data)
-    var legend = L.control({ position: "bottomright" });
-    //legend 
-    legend.onAdd = function () {
-        var div = L.DomUtil.create("div", "info legend");
-        var limits = geojson.options.limits;
-        var colors = geojson.options.colors;
-        var labels = [];
-
-        var legendInfo = "<h1>State Total Acres</h1>" +
-            "<div class=\"labels\">" +
-            "<div class=\"min\"> Min: " + limits[0] + "</div>" +
-            "<div class=\"max\"> Max: " + limits[limits.length - 1] + "</div>" +
-            "</div>";
-        div.innerHTML = legendInfo;
-
-        limits.forEach(function (limit, index) {
-            labels.push("<li style=\"background-color: " + colors[index] + "\"> </li>")
-
-        });
-        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-
-        return div;
-
-    };
-    legend.addTo(myMap);
-
+    object.push(all_features(data))
 })
-d3.json("raw_data/countyLevelGeo.json",function(county_data){
-    all_features(county_data)
+d3.json("raw_data/countyLevelGeo.json", function (county_data) {
+    object.push(all_features(county_data))
 })
 // d3.json("raw_data/countyLevelGeo.json", function(county_map){
 //     L.geoJson(county_map).addTo(myMap)
 // })
+var overlayMaps = {
+    State: object[0],
+    County: object[1]
+};
+
+L.control.layers(overlayMaps, {
+    collapsed: false
+}).addTo(myMap);
+
+var legend = L.control({ position: "bottomright" });
+//legend 
+legend.onAdd = function () {
+    var div = L.DomUtil.create("div", "info legend");
+    var limits = geojson.options.limits;
+    var colors = geojson.options.colors;
+    var labels = [];
+
+    var legendInfo = "<h1>State Total Acres</h1>" +
+        "<div class=\"labels\">" +
+        "<div class=\"min\"> Min: " + limits[0] + "</div>" +
+        "<div class=\"max\"> Max: " + limits[limits.length - 1] + "</div>" +
+        "</div>";
+    div.innerHTML = legendInfo;
+
+    limits.forEach(function (limit, index) {
+        labels.push("<li style=\"background-color: " + colors[index] + "\"> </li>")
+
+    });
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+
+    return div;
+
+};
+legend.addTo(myMap);
