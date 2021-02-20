8d530f53f4c403495b7951a46baf9ac4e2630966
BrianDouglas <bddrew86@gmail.com>
2021-02-20 11:16:17 -0800

    working on flask stuff

--- app.py
+++ app.py
@@ -4,3 +4,13 @@
         #css
     #templates
         #index.html
+import os
+import requests
+
+from flask import Flask, session, request, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index(): ,
+
