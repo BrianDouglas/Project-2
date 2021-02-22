056d1049d6b82dce10be2a0be8097985e21d054c
BrianDouglas <bddrew86@gmail.com>
2021-02-22 12:05:41 -0800

    resolving api issues

--- static/js/logic.js
+++ static/js/logic.js
@@ -33,13 +33,13 @@ function createMap(overlays){
         tileSize: 512,
         zoomOffset: -1,
         id: "mapbox/light-v9",
-        accessToken: API_KEY
+        accessToken: "pk.eyJ1IjoiYmRkcmV3ODYiLCJhIjoiY2trcm9leGk5MHN2bzJvbW9neDd1dGR6MCJ9.tio0GoaoX8K3V7WfPtbjNA"
     });
     var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "outdoors-v10",
-    accessToken: API_KEY
+    accessToken: "pk.eyJ1IjoiYmRkcmV3ODYiLCJhIjoiY2trcm9leGk5MHN2bzJvbW9neDd1dGR6MCJ9.tio0GoaoX8K3V7WfPtbjNA"
     });
     // baseMaps object to hold our base layer(s)
     var baseMaps = {
